259799a1d7108849a8a5cea0b624079a2739d81a
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-05-23 03:42:02 +0800

    added files - still needs authentication

---
+++ client/src/api/index.js
@@ -0,0 +1,11 @@
+//axios used to make API calls
+import axios from 'axios';
+
+//This url will put all the posts in our database
+const url = 'http://localhost:5000/posts';
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
